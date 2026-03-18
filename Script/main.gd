22b93215ebc078b4c37f563ea6ae2f55b57aeda3
allan <tagleallan321@gmail.com>
2026-03-19 00:26:12 +0800

    Polished Turn Based System; Added Floating Labels

--- Script/main.gd
+++ Script/main.gd
@@ -12,8 +12,11 @@ enum Enemy{
 #USER INTERFACE
 @onready var player = $Player
 @onready var enemy = $Enemy
+
+@onready var player_portrait: ColorRect = $Player/Player_Portrait
+@onready var enemy_portrait: ColorRect = $Enemy/Enemy_Portrait
+
 @onready var endTurn_button = $"Battle UI/Endturn"
-@onready var infoLabel: Label = $"Battle UI/Turn"
 @onready var flip_button = $"Battle UI/PlayerHealthBar/Flip"
 @onready var re_flip_button: Button = $"Battle UI/Re-Flip"
 @onready var turn_calculation: Label = $"Battle UI/Turn Calculation"
@@ -23,6 +26,12 @@ enum Enemy{
 
 @onready var enemy_health_bar = $"Battle UI/EnemyHealthBar"
 @onready var enemy_health_label = $"Battle UI/EnemyHealthBar/EnemyHealthLabel"
+
+@onready var turn_ui: ColorRect = $"Battle UI/Turn UI"
+@onready var turn_ui_label: Label = $"Battle UI/Turn UI/Turn UI Label"
+
+
+@onready var game_over_ui: CanvasLayer = $"Game Over UI"
 
 #COIN DECK 
 @onready var coin_deck: Node2D = $CoinDeck
@@ -41,9 +50,15 @@ var current_turn = Turn.PLAYER
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	game_over_ui.visible = false
+	turn_ui.visible = false
+	battle_start()
+	
+func battle_start():
 	randomize()
 	flip_button.pressed.connect(_on_flip_pressed)
 	endTurn_button.pressed.connect(_on_endturn_pressed)
+	re_flip_button.pressed.connect(_on_re_flip_pressed)
 	var enemy_id = randi() % 2
 	match enemy_id:
 		0: enemy.setup(Enemy.MAGE)
@@ -51,7 +66,28 @@ func _ready():
 	
 	update_enemy_coin()
 	update_player_coin()
+	show_turn_ui("BATTLE START")
 	start_player_turn()
+
+func show_turn_ui(text):
+	turn_ui.visible = true
+	turn_ui_label.text = text
+	turn_ui.modulate = Color("ffffff00")
+	turn_ui.global_position = get_viewport_rect().size / 2
+	turn_ui.global_position.x -= 600
+	
+	var target_position = turn_ui.global_position.y - 40
+	
+	var tween = create_tween()
+	tween.parallel().tween_property(turn_ui,"modulate",Color("ffffff"),0.2)
+	tween.parallel().tween_property(turn_ui, "position:y",target_position,0.2)
+	await get_tree().create_timer(1.0).timeout
+	turn_ui_label.text = text
+	tween = create_tween()
+	tween.parallel().tween_property(turn_ui,"modulate",Color("ffffff00"),0.2)
+	tween.parallel().tween_property(turn_ui, "position:y",target_position - 30,0.2)
+	await get_tree().create_timer(1.0).timeout
+	turn_ui.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -67,45 +103,49 @@ func start_player_turn():
 	debt = 0
 	
 	#Coin Gain Triggers
+	if player.gain != 0: show_floating_label(player,player.gain,LabelType.GAIN)
 	player.gain_coin()
 
 	#Reset Player Stats
 	player.current_flip = 0
 	player.current_re_flip = 0
-	
+
+	current_turn = Turn.PLAYER
+	flip_button.disabled = false
+	re_flip_button.disabled = true
+	endTurn_button.disabled = false
+	turn_calculation.text = ""
 	#Check Coin Reserve
 
 	if reserved_coin != null:
 		var coin = COIN.instantiate()
 		player.current_flip += 1
+		re_flip_button.disabled = false
 		coin.setup(reserved_coin.state,coin_deck.get_vacant_slot(player.current_flip))
+		coin.type = reserved_coin.type
 		coin.add_to_group("coins")
 		add_child(coin);
 		reserved_coin.queue_free()
 	
-	current_turn = Turn.PLAYER
-	infoLabel.text = "Player Turn"
-	flip_button.disabled = false
-	re_flip_button.disabled = true
-	endTurn_button.disabled = false
-	turn_calculation.text = ""
 	
 	
 func start_enemy_turn():
 	
+	show_turn_ui("Enemy Turn")
 	#Initialize Stats
 	damage = 0
 	gain = 0
 	debt = 0
 	
 	#Coin Gain Triggers
+	if enemy.gain != 0: show_floating_label(enemy,enemy.gain,LabelType.GAIN)
 	enemy.gain_coin()
+	
 
 	#Reset Enemy Stats
 	enemy.current_flip = 0
 	
 	current_turn = Turn.ENEMY
-	infoLabel.text = "Enemy Turn"
 	turn_calculation.text = ""
 	
 	flip_button.disabled = true
@@ -113,31 +153,39 @@ func start_enemy_turn():
 	endTurn_button.disabled = true
 	
 	#FLIP COINS
-	
+	await get_tree().create_timer(1.0).timeout
 	while enemy.current_flip != enemy.max_flip:
 		enemy_flip()
-		await get_tree().create_timer(1.0).timeout
+		await get_tree().create_timer(0.4).timeout
 	await get_tree().create_timer(1.0).timeout
 	
 	end_enemy_turn()
 
 func end_enemy_turn():
 	player.take_damage(damage)
+	if damage != 0: show_floating_label(player,damage,LabelType.DAMAGE)
 	enemy.gain += gain
+	if gain != 0: show_floating_label(enemy,gain,LabelType.TO_GAIN)
+	var defeat = check_defeat()
 	var coins = get_tree().get_nodes_in_group("enemy_coins")
 	for coin in coins:
 		coin.queue_free()
-	await get_tree().create_timer(1.0).timeout
-	check_defeat()
-	start_player_turn()
+	if defeat == null:
+		await get_tree().create_timer(0.4).timeout
+		show_turn_ui("Player Turn")
+		start_player_turn()
 
 func _on_endturn_pressed():
 	
 	enemy.take_damage(damage)
+	if damage != 0: show_floating_label(enemy,damage,LabelType.DAMAGE)
 	player.gain += gain
+	if gain != 0: show_floating_label(player,gain,LabelType.TO_GAIN)
 	reserve_left_over_coin()
-	if current_turn == Turn.PLAYER:
-		start_enemy_turn()
+	var defeat = check_defeat()
+	if defeat == null:
+		if current_turn == Turn.PLAYER:
+			start_enemy_turn()
 	var coins = get_tree().get_nodes_in_group("coins")
 	for coin in coins:
 		if coin.reserved == false:
@@ -152,6 +200,7 @@ func _on_flip_pressed():
 	var state = randi() % 2
 	player.current_flip += 1
 	player.take_damage(1)
+	show_floating_label(player,1,LabelType.DAMAGE)
 	var coin = COIN.instantiate()
 	coin.setup(state,coin_deck.get_vacant_slot(player.current_flip))
 	
@@ -161,8 +210,10 @@ func _on_flip_pressed():
 	
 	if upgrade_chance <= player.silver_flip_rate:
 		coin.upgrade_to_silver()
+		show_floating_label(player,0,LabelType.SILVER_FLIP) 
 	if upgrade_chance <= player.gold_flip_rate:
 		coin.upgrade_to_gold()
+		show_floating_label(player,0,LabelType.SILVER_FLIP) 
 	
 	coin.add_to_group("coins")
 	add_child(coin);
@@ -180,6 +231,7 @@ func enemy_flip():
 	
 	enemy.current_flip += 1
 	enemy.take_damage(1)
+	show_floating_label(enemy,1,LabelType.DAMAGE)
 	var coin = COIN.instantiate()
 	coin.setup(state,coin_deck.get_vacant_slot(enemy.current_flip))
 	
@@ -189,9 +241,12 @@ func enemy_flip():
 	
 	if upgrade_chance <= enemy.silver_flip_rate:
 		coin.upgrade_to_silver()
+		show_floating_label(enemy,0,LabelType.SILVER_FLIP) 
 	if upgrade_chance <= enemy.gold_flip_rate:
 		coin.upgrade_to_gold()
+		show_floating_label(enemy,0,LabelType.GOLD_FLIP) 
 	
+
 	coin.add_to_group("enemy_coins")
 	add_child(coin);
 
@@ -201,13 +256,18 @@ func enemy_flip():
 
 func check_defeat():
 	if player.coin <= 0:
-		infoLabel.text = "Player Defeated"
+		game_over_ui.visible = true
+		return true
 		
 	if enemy.coin <= 0:
-		infoLabel.text = "Enemy Defeated"
+		game_over_ui.visible = true
+		return false
+	
+	return null
 
 
-func _on_re_flip_pressed() -> void:
+func _on_re_flip_pressed():
+	print("REFLIP")
 	player.current_re_flip += 1
 	var coins = get_tree().get_nodes_in_group("coins")
 	for coin in coins:
@@ -305,3 +365,66 @@ func enemy_coin_calculation():
 		var text = "DMG: " + str(damage) + " GAIN: " + str(gain)
 		turn_calculation.text = text
 		turn_calculation.add_theme_color_override("font_color", Color.DARK_RED)
+
+
+enum LabelType{
+	DAMAGE,
+	GAIN,
+	TO_GAIN,
+	DEBT,
+	SILVER_FLIP,
+	GOLD_FLIP
+}
+func show_floating_label(entity, value, type):
+	var label = Label.new()
+	var target_pos
+	match type:
+		LabelType.DAMAGE:
+			label.text = "-" + str(value) + " DMG"
+			label.add_theme_color_override("font_color",Color.RED)
+		LabelType.GAIN:
+			label.text = "+" + str(value) + " GAIN"
+			label.add_theme_color_override("font_color",Color.GOLD)
+		LabelType.TO_GAIN:
+			label.text = "To GAIN: " + str(value)
+			label.add_theme_color_override("font_color",Color.DARK_GOLDENROD)
+		LabelType.SILVER_FLIP:
+			label.text = "Silver FLIP"
+			label.add_theme_color_override("font_color",Color.SILVER)
+		LabelType.GOLD_FLIP:
+			label.text = "Gold FLIP"
+			label.add_theme_color_override("font_color",Color.GOLDENROD)
+	label.add_theme_font_size_override("font_size",32)
+	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+	label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	
+	
+	if entity == player:
+		var portrait = player_portrait
+		label.global_position = portrait.global_position
+		label.global_position.x += 100
+		label.global_position.y -= 50
+		target_pos = label.global_position.y - 100
+	
+	else:
+		var portrait = enemy_portrait
+		label.global_position = portrait.global_position
+		label.global_position.x -= 20
+		label.global_position.y += 120
+		target_pos = label.global_position.y + 100
+	add_child(label)
+	
+	#FURTHER OFFSET
+	match type:
+		LabelType.TO_GAIN:
+			label.global_position.x -= 70
+		LabelType.SILVER_FLIP:
+			label.global_position.x -= 70
+			label.global_position.y -= 50
+		LabelType.GOLD_FLIP:
+			label.global_position.x -= 70
+			label.global_position.y -= 50
+	var tween = create_tween()
+	tween.parallel().tween_property(label,"position:y",target_pos,1.0)
+	tween.parallel().tween_property(label,"modulate",Color("ffffff00"),1.0)
+	tween.tween_callback(label.queue_free)

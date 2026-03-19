8b4c653a8fde0a6ae1836a06e3b2f27206483793
Vincent Casuyac <vecasuyac01202401552@usep.edu.ph>
2026-03-19 23:48:06 +0800

    Add files via upload

--- Script/player.gd
+++ Script/player.gd
@@ -9,14 +9,14 @@ var max_flip: = 18 #Max Flips Per Turn
 var current_flip: = 0: #Current Flip Count
 	set(value):
 		current_flip = clamp(value,0,max_flip)
-var max_re_flip = 6 #Max Re-Flips Per Turn
+var max_re_flip = 9 #Max Re-Flips Per Turn
 var current_re_flip = 0: #Current Re-Flip Count
 	set(value):
 		current_re_flip = clamp(value,0,max_re_flip)
-var silver_flip_rate = 0.3: #Chance to Flip a Silver Coin
+var silver_flip_rate = 0.2: #Chance to Flip a Silver Coin
 	set(value): 
 		silver_flip_rate = clamp(value,0.0,100.0) 
-var gold_flip_rate = 0.05: #Chance to Flip a Gold Coin
+var gold_flip_rate = 0.1: #Chance to Flip a Gold Coin
 	set(value): 
 		gold_flip_rate = clamp(value,0.0,100.0) 
 

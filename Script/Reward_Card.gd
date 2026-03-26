9381bf5a287329de22c523d9ee69395ac4fa1805
Vincent Casuyac <vecasuyac01202401552@usep.edu.ph>
2026-03-26 23:46:07 +0800

    Added Passive Manger- Notification

--- Script/Reward_Card.gd
+++ Script/Reward_Card.gd
@@ -2,22 +2,20 @@ extends Button
 
 @export var card_id : int
 @export var card_name : String
-@export var card_tier: String = "Copper"
-
 
 signal card_selected(card_id)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	text = card_name + " (" + card_tier + ")"
-	self.pressed.connect(_on_pressed)
+	text = "%s" % [card_name]
+	if not pressed.is_connected(_on_pressed):
+		pressed.connect(_on_pressed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 func _on_pressed() -> void:
-	print("CLICKED:", card_name, "-", card_tier)
 	emit_signal("card_selected", card_id)
--- Script/player.gd
+++ Script/player.gd
@@ -1,14 +1,11 @@
 extends Node2D
 
-signal hp_changed(new_hp)
-
 #PLAYER STATS
-var max_coin = 500 #Max Coin Capacity
-var coin = 59:
+var max_coin = 100 #Max Coin Capacity
+var coin = 100:
 	set(value):
 		coin = clamp(value,0,max_coin)
-		hp_changed.emit(coin)
-var max_flip: = 16 #Max Flips Per Turn
+var max_flip: = 18 #Max Flips Per Turn
 var current_flip: = 0: #Current Flip Count
 	set(value):
 		current_flip = clamp(value,0,max_flip)
@@ -16,30 +13,23 @@ var max_re_flip = 9 #Max Re-Flips Per Turn
 var current_re_flip = 0: #Current Re-Flip Count
 	set(value):
 		current_re_flip = clamp(value,0,max_re_flip)
-var silver_flip_rate = 0.1: #Chance to Flip a Silver Coin
+var silver_flip_rate = 0.2: #Chance to Flip a Silver Coin
 	set(value): 
 		silver_flip_rate = clamp(value,0.0,100.0) 
-var gold_flip_rate = 0.05: #Chance to Flip a Gold Coin
+var gold_flip_rate = 0.1: #Chance to Flip a Gold Coin
 	set(value): 
 		gold_flip_rate = clamp(value,0.0,100.0) 
 
 #STATUS EFFECTS
 
-var gain = 0: #Coin to be gained next turn
-	set(value):
-		gain = clamp(value,0,1000) 
-var debt = 0: #Gain Blocked
-	set(value):
-		debt = clamp(value,0,1000) 
+var gain = 0 #Coin to be gained next turn
+var debt = 0 #Damage to be receieved
 
 func take_damage(amount):
 	coin -= amount
 	print("Player HP: ", coin)
 
 func gain_coin():
-	var temp = gain
-	gain -= debt
-	debt -= temp
 	coin += gain
 	gain = 0
 	print("Player HP: ", coin)
@@ -47,4 +37,9 @@ func gain_coin():
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
 	pass

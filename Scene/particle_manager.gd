1d526f2d213eb992580080c316ab190fbdcb7725
allan <tagleallan321@gmail.com>
2026-03-28 04:50:55 +0800

    null error fixed?

--- Scene/particle_manager.gd
+++ Scene/particle_manager.gd
@@ -6,9 +6,6 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func spawn_particle(p,pos):
 	if is_instance_valid(p):
